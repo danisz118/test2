abe1ded1fdb9797057af93413cddce44acba91a3
danisz118 <danisz118@gmail.com>
2019-09-10 16:06:37 +0300

    Убрали лишнее добавил игру полную

--- src/Test/Bingo.java
+++ src/Test/Bingo.java
@@ -1,4 +1,49 @@
 package Test;
 
+import java.util.Random;
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
+/*
+Написать программу игру “Bingo”.
+Bingo.
+Программа должна загадать целое число в диапазоне от 0 до 100 и предложить пользователю
+его отгадать.
+При вводе числа пользователем, программа проверяет на соответствие с загаданным число и
+если числа совпали вывести сообщение о том что число отгадано. Если числа не совпали, тогда
+следует вывести надпись о том, что задуманное число является больше или меньше вводимого.
+Также программа ведет подсчет попыток, и выводит это количество после того как число
+угадали. Предусмотреть возможность досрочного завершения программы, в случае если
+пользователь не желает продолжать угадывать число.
+ */
 public class Bingo {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int numGueses = 0;   // переменная догадок
+        int randomNumber = new Random() .nextInt(10)+1;
+        out.println("Добро пожаловать в игру");
+        out.println();
+        out.println("Введите любое чило от 1 до 10");
+        int inputNumber = keyboard.nextInt();
+        numGueses++;
+
+        while (inputNumber != randomNumber){
+            out.println();
+            out.println("Попытайтесь еще раз ..");
+            out.println("Введите любое чило от 1 до 10");
+            inputNumber = keyboard.nextInt();
+            numGueses++;
+            if (inputNumber < randomNumber){
+                System.out.println("Задуманное число больше вводимого ");
+            }
+            else if (inputNumber > randomNumber){
+                System.out.println("Задуманное число меньше  вводимого ");
+
+            }
+        }
+        out.println("Вы угадали число после  ");
+        out.println(numGueses + "  попыток");
+    }
+
 }
--- src/Test/massSolution2.java
+++ src/Test/massSolution2.java
@@ -1,31 +1,29 @@
 package Test;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class massSolution2 {
-    public static void main(String[] args) {
-        BufferedReader reader  = new BufferedReader(new InputStreamReader(System.in));
-        int bigarray [] = new  int [20];
-        for (int i = 0; i <bigarray.length; i++){
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int bigarray[] = new int[20];
+        for (int i = 0; i < bigarray.length; i++) {
             int a = Integer.parseInt(reader.readLine());
-            bigarray [i] = a;
+            bigarray[i] = a;
         }
-        int array1 [] = new int [10];
-        int array2 [] = new int[10];
+        int array1[] = new int[10];
+        int array2[] = new int[10];
 
-        array1 = Arrays.copyOfRange(bigarray,0,10);
-        array2 = Arrays.copyOfRange(bigarray,10,20);
+        array1 = Arrays.copyOfRange(bigarray, 0, 10);
+        array2 = Arrays.copyOfRange(bigarray, 10, 20);
 
-        for (int j = 0; j < array2.length - 1; j++){
+        for (int j = 0; j < array2.length - 1; j++) {
             System.out.println(array2[j]);
-
+        }
     }
-
-
 }
-
 /*
 1. Создать массив на 20 чисел.
 2. Ввести в него значения с клавиатуры.
